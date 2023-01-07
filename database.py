71797533fb92a5630b8e2d995c3a433bb3439b85
Corey Wenger <corey.wenger@gmail.com>
2023-01-07 11:42:55 -0500

    Added a reset function to the database module.

--- database.py
+++ database.py
@@ -1,5 +1,13 @@
 import sqlite3
 
+db_create_statement = """CREATE TABLE READING (
+    READ_ID INT AUTO_INCREMENT,
+    READ_SENSOR_NUM INT NOT NULL,
+    READ_HUMIDITY DECIMAL(3, 2) NOT NULL,
+    READ_TEMP DECIMAL(3, 2) NOT NULL,
+    READ_DATE DATETIME NOT NULL,
+    PRIMARY KEY (READ_ID))"""
+
 class Database:
     def __init__(self, config):
         self.db_name = config["database_name"]
@@ -8,12 +16,15 @@ class Database:
         con = sqlite3.connect(self.db_name)
         cur = con.cursor()
 
-        cur.execute("""CREATE TABLE READING (
-            READ_ID INT AUTO_INCREMENT,
-            READ_SENSOR_NUM INT NOT NULL,
-            READ_HUMIDITY DECIMAL(3, 2) NOT NULL,
-            READ_TEMP DECIMAL(3, 2) NOT NULL,
-            READ_DATE DATETIME NOT NULL,
-            PRIMARY KEY (READ_ID))""")
+        cur.execute(db_create_statement)
     
+        con.close()
+
+    def reset_readings_table(self):
+        con = sqliite3.connect(self.db_name)
+        cur = con.cursor()
+
+        cur.execute("DROP TABLE READING")
+        cur.execute(db_create_statement)
+
         con.close()
