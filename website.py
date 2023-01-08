2eb39d8102c5240d3250982bc5fc50d656f40b2e
Levi Wenger <myeyesareonmizar@gmail.com>
2023-01-07 21:34:37 -0500

    Added website template.

--- website.py
+++ website.py
@@ -1,18 +1,28 @@
 import json
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from humidity_reader import read_humidity, read_temp
 from database import Database
 from http.server import HTTPServer, BaseHTTPRequestHandler
+
+config_file_name = "config.json"
+config_file = open("config.json")
+config = json.load(config_file)
+config_file.close()
+
+db = Database(config)
+env = Environment(
+    loader=FileSystemLoader("."),
+    autoescape=select_autoescape()
+)
+
+template = env.get_template(config["webserver_template_file"])
 
 class Website(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("hello world!", "utf-8"))
-
-config_file_name = "config.json"
-config_file = open("config.json")
-config = json.load(config_file)
-config_file.close()
+        self.wfile.write(bytes(template.render(inst_temp=read_temp(), inst_humidity=read_humidity()), "utf-8"))
 
 print("Starting webserver...")
 website = HTTPServer((config["webserver_hostname"], config["webserver_port"]), Website)
